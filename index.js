5e4f18c1b39951103a4397218e65fe557ff5ee26
Shakira Hadjih <shakira.hadjih@hva.nl>
2021-05-04 17:49:37 +0200

    added 404 message

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.get('/movies', (req, res) => {
     res.send('<h1>This will become a list of movies</h1>')
-  })
+  });
 
   app.get('/movies/:movieId/:slug', (req, res) => {
     res.send(`<h1>This will become a detail page for ${req.params.slug}</h1>`)
-  })
+  });
+
+  app.use(function (req, res, next) {
+      res.status(404).send("Sorry can't find that!")
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
